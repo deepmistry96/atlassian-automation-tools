2cd4ac489d7063abb4e6bf765998c904ede28654
testbed_ultimate <testbed_ultimate@2603-8080-5602-3313-c0e9-164a-94bb-ec83.res6.spectrum.com>
2024-04-08 20:17:57 -0500

    very terrible mvp

--- jira-api-playground.py
+++ jira-api-playground.py
@@ -1,6 +1,10 @@
 from jira import JIRA
 import os
 
+import pdb
+# pdb.set_trace()
+
+
 users_dict = {
                     "deep"  : "Deep Mistry",
                     "malav" : "Malav Chauhan",
@@ -23,10 +27,10 @@ def connect_to_jira():
                 )
     issue = jira.issue('AE-68', fields='summary,comment')
     
-    print("Printing test issue")
+    
 
     try:
-        print(issue)
+        print("testuing success")
     except:
         print("Failed to grab the issue")
 
@@ -49,17 +53,17 @@ def get_jira_issue_specifics(jira, issue_str):
         "issue_key"  : issue_obj.key,
         "issue_summary" : issue_obj.fields.summary,
         "issue_type" : issue_obj.fields.issuetype,
-        "issue_parent" : issue_obj.fields.parent,
+        # "issue_parent" : issue_obj.fields.parent,
         "issue_project" : issue_obj.fields.project,
         "issue_resolution" : issue_obj.fields.resolution,
-        "issue_resolutiondata" : issue_obj.fields.resolutiondate,
-        "issue_priority" : issue_obj.fields.priority,
+        # "issue_resolutiondata" : issue_obj.fields.resolutiondate,
+        # "issue_priority" : issue_obj.fields.priority,
         "issue_assignee" : issue_obj.fields.assignee,
         "issue_status" : issue_obj.fields.status,
-        "issue_timeoriginalestimate" : issue_obj.fields.timeoriginalestimate,
-        "issue_timeestimate" : issue_obj.fields.timeestimate,
+        # "issue_timeoriginalestimate" : issue_obj.fields.timeoriginalestimate,
+        # "issue_timeestimate" : issue_obj.fields.timeestimate,
         "issue_description" : issue_obj.fields.description,
-        "issue_progress" : issue_obj.fields.progress,
+        # "issue_progress" : issue_obj.fields.progress,
         "issue_summary" : issue_obj.fields.summary,
         "issue_timespent" : issue_obj.fields.timespent,
     }
@@ -83,38 +87,53 @@ def get_jira_issue_specifics(jira, issue_str):
 
 
 def list_completed_issues(jira):
-    comepleted_issues_in_proj = jira.search_issues('project = "AE" AND status = "Met Scope"') #AND assignee = "Deep" ORDER BY created DESC')
+    comepleted_issues_in_proj = jira.search_issues('project = "AE" AND status = "Think I\'ve met scope.()"') #AND assignee = "Deep" ORDER BY created DESC')
     
     completed_issues_list = []
     
     for iter in comepleted_issues_in_proj:
-        print(iter.key)
         completed_issues_list.append(iter.key)
 
     return completed_issues_list
 
 
-def return_reducedlist_assignedto_user(jira, list_of_issues, user):
+# def return_reducedlist_assignedto_user(jira, issue_list, user):
+def return_reducedlist_assignedto_user(jira, user):
     user_specific_issues = []
+    # # issue_list.
+    # for i in issue_list(jira):
+    #     issue_specifics = get_jira_issue_specifics(jira, i)
+    #     if issue_specifics["issue_assignee"] == user:
+    #         user_specific_issues.append(i)
 
-    for i in list_of_issues:
-        issue_specifics = get_jira_issue_specifics(jira, i)
-        if issue_specifics.issue_assignee == user:
-            user_specific_issues.append(i)
+    search_query = ('project = "AE" AND status = "Think I\'ve met scope.()" AND assignee = "{}"') #TODO: Make sure this string formatting works
+    
+    user_specific_issues = jira.search_issues(search_query.format(user))
           
-    return user_specific_issues
+    return user_specific_issues 
 
-def prettyprint_issuespecifics_and_totaltime(jira, list_of_issues):
+def prettyprint_issuespecifics_and_totaltime(jira, issue_list):
     totaltime = 0
-    for i in list_of_issues:
-        print("user:{0} , completed:{1} , in {2} hours".format(
-                                                            i["issue_assignee"], 
-                                                            i["issue_key"], 
-                                                            i["issue_timespent"], 
-                                                            ))
-        totaltime = totaltime + i["issue_timespent"]
+    for i in issue_list:
 
-    print("Total time was: " + totaltime)
+        issue_specifics = get_jira_issue_specifics(jira, i)
+        
+        if issue_specifics["issue_timespent"] == None:
+            issue_specifics["issue_timespent"] = 0
+
+        timespent_string = str(issue_specifics["issue_timespent"])
+
+
+        print("user:{0} , completed:{1} , in {2} hours".format(
+                                                            issue_specifics["issue_assignee"], 
+                                                            issue_specifics["issue_key"], 
+                                                            timespent_string, 
+                                                            ))
+        
+        totaltime = totaltime + issue_specifics["issue_timespent"]
+
+    timespent_string = str(totaltime)
+    print("Total time was: " + timespent_string)
 
 jira = connect_to_jira()
 issue_obj = get_jira_issue(jira, "AE-94")
@@ -126,13 +145,13 @@ issues_in_proj = jira.search_issues('project=AE')
 
 #project = "AE" AND status = "Met Scope"  AND assignee != "Deep" ORDER BY created DESC
 # project = "AE" AND status = "In Progress"  AND assignee != "Deep" ORDER BY created DESC
-comepleted_issues_in_proj = jira.search_issues('project = "AE" AND status = "Met Scope"') #AND assignee = "Deep" ORDER BY created DESC')
+# comepleted_issues_in_proj = jira.search_issues('project = "AE" AND status = "Met Scope"') #AND assignee = "Deep" ORDER BY created DESC')
 list_of_completed_issues = list_completed_issues(jira)
 
 
-alexs_issues = return_reducedlist_assignedto_user(jira, list_completed_issues, users_dict['alex'])
+alexs_issues_list = return_reducedlist_assignedto_user(jira, users_dict['deep'])
+prettyprint_issuespecifics_and_totaltime(jira, alexs_issues_list)
+#prettyprint_issuespecifics_and_totaltime(jira, return_reducedlist_assignedto_user(jira, list_completed_issues, users_dict['deep']))
 
 
-import pdb
-pdb.set_trace()
 
